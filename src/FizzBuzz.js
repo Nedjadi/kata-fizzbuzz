ed8c9ccc53f8ef27f7f0206dbe2fbdb726d66ca7
Nedjadi <ahmed.nedjadi@laposte.netgit config --global user.name Nedjadigit statusgit confighelpcd devlscd devgit config --global user.email ahmed.nedjadi@laposte.net>
2015-11-27 09:42:53 +0100

    kata FizzBuzz Done sooo Well YAY !

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,18 @@
-function fizzBuzz(i) {
-  
+function fizzBuzz(i){
+  if (i % 15 == 0){
+    return "FizzBuzz";
+  }
+
+  else if(i % 5 == 0){
+    return "Buzz";
+  }
+
+  else if(i % 3 == 0){
+    return "Fizz";
+  }
+  else{
+    return(i);
+  }
 }
 
 module.exports = fizzBuzz;
